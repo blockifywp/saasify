df17f2035f3db6f84433b8a2e4af6e43bc535c88
SEO Themes <seothemeswp@gmail.com>
2024-03-07 19:31:17 +0800

    patterns

--- patterns/cta/box.php
+++ patterns/cta/box.php
@@ -1,17 +1,15 @@
 <?php
 /**
- * Title: Box
+ * Title: Cta Box
  * Slug: box
  * Categories: cta
- * ID: 15
  */
 ?>
-<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","style":{"u002du002dposition":"relative","border":{"radius":"12px"},"position":{"all":"relative"}},"className":"is-style-default","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide is-style-default" style="border-radius:12px"><!-- wp:spacer {"height":"400px","style":{"filter":{"opacity":"20","blur":120},"position":{"all":"absolute"},"width":{"all":"800px"},"right":{"all":"-200px"},"top":{"all":"-100px"},"bottom":{"all":""},"left":{"all":""},"transform":{"rotate":23},"zIndex":{"all":0}},"gradient":"vivid-cyan-blue-to-vivid-purple"} -->
-<div style="height:400px;filter:blur(120px) opacity(20%);transform:rotate(23deg)" aria-hidden="true" class="wp-block-spacer has-vivid-cyan-blue-to-vivid-purple-gradient-background has-background"></div>
+<!-- wp:group {"align":"full","style":{"position":{"all":"relative"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:spacer {"height":"100%","style":{"position":{"all":"absolute"},"width":{"all":"50%"},"maxWidth":{"all":"50%"},"filter":{"blur":"80","opacity":"20"},"top":{"all":""},"right":{"all":""},"bottom":{"all":""},"left":{"all":"50%"}},"gradient":"vivid-cyan-blue-to-vivid-purple"} -->
+<div style="height:100%;filter:blur(80px) opacity(20%)" aria-hidden="true" class="wp-block-spacer has-vivid-cyan-blue-to-vivid-purple-gradient-background has-background"></div>
 <!-- /wp:spacer -->
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|sm","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|sm"}},"u002du002dposition":"relative","u002du002dz-index":"2","border":{"radius":"30px"},"position":{"all":"relative"},"zIndex":{"all":"1"}},"backgroundColor":"surface","className":"is-style-default","layout":{"type":"constrained","contentSize":"640px"}} -->
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|sm","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|sm"}},"u002du002dposition":"relative","u002du002dz-index":"2","border":{"radius":"30px"},"position":{"all":"relative"},"zIndex":{"all":"1"}},"backgroundColor":"surface","className":"is-style-default","layout":{"type":"default"}} -->
 <div class="wp-block-group is-style-default has-surface-background-color has-background" style="border-radius:30px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--sm)"><!-- wp:heading {"textAlign":"center"} -->
 <h2 class="wp-block-heading has-text-align-center">Ready to take your site the next level?</h2>
 <!-- /wp:heading -->
@@ -26,6 +24,5 @@
 <div class="wp-block-button has-box-shadow is-style-outline"><a class="wp-block-button__link has-heading-color has-text-color wp-element-button" href="#">Learn More</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
-<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
